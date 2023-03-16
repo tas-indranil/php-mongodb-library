3c83073dc97873b9775ccc25271053c6f2d79595
Srinivas-TAS <116160075+Srinivas-TAS@users.noreply.github.com>
2023-03-16 13:02:19 +0530

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -5,7 +5,7 @@ author=$(git log -1 --pretty=format:'%an')
 echo $author
 if [ "$Test_Res" -le "100" ]
   then
-    echo "The test cases results matched security criteria.."
+    echo "The test cases results matched security criteria"
   else
     echo "The test cases didn't matched security criteria please check....!" && aws sns publish --topic-arn arn:aws:sns:us-east-2:651833712667:totalai_dev --message "The $GIT_PREVIOUS_COMMIT submitted by '${author}' does not match the required security standard to be meet."  && exit 1;
 fi
